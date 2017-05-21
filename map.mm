a6e28c7c6e54bb4be0f29de974f44c014ba9dbda
Dennis Siqueira <ti.dennissiqueira@gmail.com>
2017-05-21 13:40:09 -0300

    Version 1.1
    add effects, exercises page with mask, cv, triangles, etc.
    +10 functions 10/15 items for project

---
+++ map.mm
@@ -0,0 +1,221 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="dennissiq.github.io" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1749989795" CREATED="1495314313563" MODIFIED="1495318854075"><hook NAME="MapStyle" zoom="0.77">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="22" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Menu (navbar menu)" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" FOLDED="true" POSITION="right" ID="ID_1800352355" CREATED="1495314721968" MODIFIED="1495317274234" HGAP_QUANTITY="31.249999485909957 pt" VSHIFT_QUANTITY="-43.49999870359902 pt">
+<node TEXT="Home" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_1886242215" CREATED="1495314740485" MODIFIED="1495317771264" HGAP_QUANTITY="85.99999785423284 pt" VSHIFT_QUANTITY="13.499999597668662 pt">
+<node TEXT="Vai para a p&#xe1;gina inicial." ID="ID_1810679948" CREATED="1495315012610" MODIFIED="1495315153732" HGAP_QUANTITY="54.499998793005986 pt" VSHIFT_QUANTITY="-86.24999742954978 pt"/>
+</node>
+<node TEXT="Sobre" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_520496234" CREATED="1495314787716" MODIFIED="1495317771263" HGAP_QUANTITY="104.74999729543933 pt" VSHIFT_QUANTITY="28.499999150633826 pt">
+<node TEXT="Vai para p&#xe1;gina &quot;Sobre&quot;." ID="ID_1248173757" CREATED="1495315036991" MODIFIED="1495315138147" HGAP_QUANTITY="72.4999982565642 pt" VSHIFT_QUANTITY="-39.74999881535772 pt"/>
+</node>
+<node TEXT="Contato" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_265445417" CREATED="1495314806718" MODIFIED="1495317771262" HGAP_QUANTITY="131.74999649077665 pt" VSHIFT_QUANTITY="32.99999901652339 pt">
+<node TEXT="Rola a p&#xe1;gina at&#xe9; a parte de contato." ID="ID_1382895327" CREATED="1495315163054" MODIFIED="1495315185695" HGAP_QUANTITY="61.99999856948857 pt" VSHIFT_QUANTITY="-69.74999792128807 pt"/>
+</node>
+<node TEXT="Fotografia" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_979643787" CREATED="1495314915547" MODIFIED="1495317771262" HGAP_QUANTITY="91.99999767541894 pt" VSHIFT_QUANTITY="68.99999794363978 pt">
+<node TEXT="Vai para p&#xe1;gina &quot;Fotografia&quot;." FOLDED="true" ID="ID_596593912" CREATED="1495315201298" MODIFIED="1495316114928" HGAP_QUANTITY="109.24999716132888 pt" VSHIFT_QUANTITY="56.99999830126768 pt">
+<node TEXT="Acesso a galeria de fotos (lightbox)." ID="ID_905418274" CREATED="1495315433577" MODIFIED="1495316114927" HGAP_QUANTITY="31.99999946355821 pt" VSHIFT_QUANTITY="-47.24999859184031 pt"/>
+</node>
+</node>
+<node TEXT="Exerc&#xed;cios" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_27711568" CREATED="1495314799058" MODIFIED="1495317771258" HGAP_QUANTITY="55.9999987483025 pt" VSHIFT_QUANTITY="-81.74999756366023 pt">
+<node TEXT="Vai para p&#xe1;gina &quot;Exerc&#xed;cios&quot;." ID="ID_404382725" CREATED="1495316096067" MODIFIED="1495316112299" HGAP_QUANTITY="89.74999774247415 pt" VSHIFT_QUANTITY="89.99999731779107 pt"/>
+</node>
+<node TEXT="Logo do Menu" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_1651719875" CREATED="1495317684043" MODIFIED="1495317787023">
+<node TEXT="Vai para P&#xe1;gina &quot;Home&quot;" ID="ID_943640255" CREATED="1495317782434" MODIFIED="1495317787023" HGAP_QUANTITY="56.74999872595075 pt" VSHIFT_QUANTITY="61.49999816715723 pt"/>
+</node>
+</node>
+<node TEXT="P&#xe1;gina &quot;Home&quot;" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" FOLDED="true" POSITION="left" ID="ID_674635811" CREATED="1495315579054" MODIFIED="1495317015944" HGAP_QUANTITY="57.49999870359901 pt" VSHIFT_QUANTITY="-73.49999780952938 pt">
+<node TEXT="Rolar P&#xe1;gina" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_1747354476" CREATED="1495315868848" MODIFIED="1495317721239" HGAP_QUANTITY="71.74999827891594 pt" VSHIFT_QUANTITY="-20.249999396502982 pt">
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Contato&quot;" FOLDED="true" ID="ID_949795358" CREATED="1495316489303" MODIFIED="1495316524669">
+<node TEXT="Abre um e-mail." ID="ID_1024574218" CREATED="1495318628258" MODIFIED="1495318644938" HGAP_QUANTITY="51.499998882412946 pt" VSHIFT_QUANTITY="-32.99999901652339 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Google Maps&quot;" FOLDED="true" ID="ID_1724202530" CREATED="1495316490268" MODIFIED="1495316508775">
+<node TEXT="Abre mapa com localiza&#xe7;&#xe3;o da Universidade." ID="ID_1843715293" CREATED="1495318573728" MODIFIED="1495318624662" HGAP_QUANTITY="96.49999754130847 pt" VSHIFT_QUANTITY="-47.24999859184031 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Social Media Buttons&quot;" FOLDED="true" ID="ID_1694636392" CREATED="1495316491712" MODIFIED="1495318730698" VSHIFT_QUANTITY="0.7499999776482589 pt">
+<node ID="ID_1488321639" CREATED="1495318726694" MODIFIED="1495318730698" HGAP_QUANTITY="58.999998658895535 pt" VSHIFT_QUANTITY="25.4999992400408 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Abre Redes Sociais.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Bot&#xe3;o para ir no &quot;top&quot; da p&#xe1;gina" ID="ID_1090938994" CREATED="1495317722986" MODIFIED="1495317724629"/>
+</node>
+</node>
+<node TEXT="P&#xe1;gina &quot;Sobre&quot;" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" FOLDED="true" POSITION="right" ID="ID_1095214167" CREATED="1495316381988" MODIFIED="1495318854075" HGAP_QUANTITY="11.000000089406967 pt" VSHIFT_QUANTITY="266.2499920651319 pt">
+<node TEXT="Rolar P&#xe1;gina" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_470238712" CREATED="1495316403448" MODIFIED="1495317259628" HGAP_QUANTITY="25.24999966472388 pt" VSHIFT_QUANTITY="36.74999890476469 pt">
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Contato&quot;" FOLDED="true" ID="ID_29682785" CREATED="1495316419095" MODIFIED="1495318739710">
+<node TEXT="Abre um e-mail." ID="ID_953606264" CREATED="1495318681857" MODIFIED="1495318739710" HGAP_QUANTITY="31.99999946355821 pt" VSHIFT_QUANTITY="-89.99999731779107 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Google Maps&quot;" FOLDED="true" ID="ID_413327904" CREATED="1495316455120" MODIFIED="1495316469148">
+<node TEXT="Abre mapa com localiza&#xe7;&#xe3;o da Universidade." ID="ID_1783048789" CREATED="1495318573728" MODIFIED="1495318624662" HGAP_QUANTITY="96.49999754130847 pt" VSHIFT_QUANTITY="-47.24999859184031 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Social Media Buttons&quot;" FOLDED="true" ID="ID_1457225973" CREATED="1495316469410" MODIFIED="1495316482206">
+<node TEXT="Abre Redes Sociais." ID="ID_77158943" CREATED="1495318553625" MODIFIED="1495318602438" HGAP_QUANTITY="69.49999834597116 pt" VSHIFT_QUANTITY="44.999998658895535 pt"/>
+</node>
+<node TEXT="Bot&#xe3;o para ir no &quot;top&quot; da p&#xe1;gina" ID="ID_501701935" CREATED="1495317745091" MODIFIED="1495317746940"/>
+</node>
+</node>
+<node TEXT="P&#xe1;gina &quot;Fotografia&quot;" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" FOLDED="true" POSITION="left" ID="ID_64774253" CREATED="1495316542104" MODIFIED="1495317236272" HGAP_QUANTITY="72.4999982565642 pt" VSHIFT_QUANTITY="34.49999897181991 pt">
+<node TEXT="Rolar P&#xe1;gina" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_1884890714" CREATED="1495316946375" MODIFIED="1495317495710" HGAP_QUANTITY="97.99999749660499 pt" VSHIFT_QUANTITY="44.24999868124728 pt">
+<node TEXT="Galeria de Fotos" FOLDED="true" ID="ID_30892600" CREATED="1495317139196" MODIFIED="1495317896326">
+<node TEXT="Abre &quot;Lightbox&quot;" ID="ID_1734293781" CREATED="1495317157316" MODIFIED="1495317896325" HGAP_QUANTITY="22.999999731779106 pt" VSHIFT_QUANTITY="-45.74999863654379 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Contato&quot;" FOLDED="true" ID="ID_1281643175" CREATED="1495317144110" MODIFIED="1495317193229">
+<node TEXT="Abre um e-mail." ID="ID_728722524" CREATED="1495318628258" MODIFIED="1495318644938" HGAP_QUANTITY="51.499998882412946 pt" VSHIFT_QUANTITY="-32.99999901652339 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Google Maps&quot;" FOLDED="true" ID="ID_1099426229" CREATED="1495317147332" MODIFIED="1495317205366">
+<node TEXT="Abre mapa com localiza&#xe7;&#xe3;o da Universidade." ID="ID_1836225645" CREATED="1495318573728" MODIFIED="1495318624662" HGAP_QUANTITY="96.49999754130847 pt" VSHIFT_QUANTITY="-47.24999859184031 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Social Media Buttons&quot;" FOLDED="true" ID="ID_1015226768" CREATED="1495317148839" MODIFIED="1495318719728">
+<node ID="ID_1172884600" CREATED="1495318715088" MODIFIED="1495318719728" HGAP_QUANTITY="114.49999700486669 pt" VSHIFT_QUANTITY="44.999998658895535 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Abre Redes Sociais.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Bot&#xe3;o para ir no &quot;top&quot; da p&#xe1;gina" ID="ID_15533600" CREATED="1495317496843" MODIFIED="1495317514330"/>
+</node>
+</node>
+<node TEXT="P&#xe1;gina &quot;Exerc&#xed;cios&quot;" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" FOLDED="true" POSITION="left" ID="ID_923728552" CREATED="1495317057416" MODIFIED="1495317625974" HGAP_QUANTITY="124.99999669194231 pt" VSHIFT_QUANTITY="154.49999539554133 pt">
+<node TEXT="Rolar P&#xe1;gina" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_1150172212" CREATED="1495317387276" MODIFIED="1495317410988" HGAP_QUANTITY="50.74999890476469 pt" VSHIFT_QUANTITY="80.2499976083637 pt">
+<node TEXT="Exerc&#xed;cios" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" FOLDED="true" ID="ID_1296200438" CREATED="1495317470855" MODIFIED="1495318005246">
+<node TEXT="Menu dos Exerc&#xed;cios &quot;Tab Headers&quot;" LOCALIZED_STYLE_REF="AutomaticLayout.level,4" FOLDED="true" ID="ID_819681593" CREATED="1495317922843" MODIFIED="1495318005245" HGAP_QUANTITY="86.74999783188112 pt" VSHIFT_QUANTITY="-81.74999756366022 pt">
+<node TEXT="Curr&#xed;culo 1" FOLDED="true" ID="ID_990867655" CREATED="1495318025294" MODIFIED="1495318146226">
+<node TEXT="Abre o exerc&#xed;cio de Curr&#xed;culo formatado usando CSS inline." ID="ID_845987665" CREATED="1495318140441" MODIFIED="1495318264803" HGAP_QUANTITY="84.49999789893633 pt" VSHIFT_QUANTITY="-47.24999859184031 pt"/>
+</node>
+<node TEXT="Curr&#xed;culo 2" FOLDED="true" ID="ID_1847469450" CREATED="1495318030745" MODIFIED="1495318183910">
+<node TEXT="Abre o exerc&#xed;cio de Curr&#xed;culo utilizando CSS externo." ID="ID_407233197" CREATED="1495318163968" MODIFIED="1495318280988" HGAP_QUANTITY="120.49999682605278 pt" VSHIFT_QUANTITY="-37.499998882412946 pt"/>
+</node>
+<node TEXT="Curr&#xed;culo 3" FOLDED="true" ID="ID_218627590" CREATED="1495318061994" MODIFIED="1495318189724">
+<node TEXT="Abre o exerc&#xed;cio de Curr&#xed;culo utilizando efeitos JQuery." ID="ID_610355988" CREATED="1495318186770" MODIFIED="1495319792430" HGAP_QUANTITY="110.7499971166254 pt" VSHIFT_QUANTITY="43.49999870359901 pt"/>
+</node>
+<node TEXT="Curr&#xed;culo 4" FOLDED="true" ID="ID_1199484068" CREATED="1495318031503" MODIFIED="1495318214094">
+<node TEXT="Abre o exerc&#xed;cio de Curr&#xed;culo utilizando efeitos de CSS diferentes." ID="ID_334348629" CREATED="1495318211154" MODIFIED="1495318328202" HGAP_QUANTITY="87.49999780952938 pt" VSHIFT_QUANTITY="72.74999783188112 pt"/>
+</node>
+<node TEXT="Tri&#xe2;ngulo" FOLDED="true" ID="ID_856350611" CREATED="1495318032827" MODIFIED="1495318337594">
+<node TEXT="Abre o exerc&#xed;cio de calcular tri&#xe2;ngulos." FOLDED="true" ID="ID_693077459" CREATED="1495318335594" MODIFIED="1495318399928" HGAP_QUANTITY="97.24999751895673 pt" VSHIFT_QUANTITY="79.49999763071544 pt">
+<node TEXT="Calcula e classifica os tri&#xe2;ngulos ap&#xf3;s dado o valor." ID="ID_932524065" CREATED="1495318393218" MODIFIED="1495318399927" HGAP_QUANTITY="88.24999778717763 pt" VSHIFT_QUANTITY="65.24999805539852 pt"/>
+</node>
+</node>
+<node TEXT="Banner" FOLDED="true" ID="ID_307906902" CREATED="1495318034825" MODIFIED="1495318411976">
+<node TEXT="Sequ&#xea;ncia de imagens utilizando JS." ID="ID_521774769" CREATED="1495318409316" MODIFIED="1495318414237" HGAP_QUANTITY="170.7499953284861 pt" VSHIFT_QUANTITY="62.999998122453746 pt"/>
+</node>
+<node TEXT="M&#xe1;scaras" FOLDED="true" ID="ID_1951446346" CREATED="1495318036158" MODIFIED="1495318418746">
+<node TEXT="Utiliza&#xe7;&#xe3;o de m&#xe1;scaras em formul&#xe1;rios com JS." ID="ID_1631465879" CREATED="1495318416968" MODIFIED="1495318424879" HGAP_QUANTITY="101.74999738484628 pt" VSHIFT_QUANTITY="73.49999780952938 pt"/>
+</node>
+<node TEXT="Mais" FOLDED="true" ID="ID_760818759" CREATED="1495318038397" MODIFIED="1495318447019">
+<node TEXT="Mostra uma contagem regressiva utilizando &quot;CountDown Timer&quot;" ID="ID_179889058" CREATED="1495318438606" MODIFIED="1495318497521" HGAP_QUANTITY="106.24999725073585 pt" VSHIFT_QUANTITY="91.49999727308759 pt"/>
+</node>
+</node>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Contato&quot;" FOLDED="true" ID="ID_793884955" CREATED="1495317474676" MODIFIED="1495318644938">
+<node TEXT="Abre um e-mail." ID="ID_86463525" CREATED="1495318628258" MODIFIED="1495318644938" HGAP_QUANTITY="51.499998882412946 pt" VSHIFT_QUANTITY="-32.99999901652339 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Google Maps&quot;" FOLDED="true" ID="ID_1792721377" CREATED="1495317477058" MODIFIED="1495318593548">
+<node TEXT="Abre mapa com localiza&#xe7;&#xe3;o da Universidade." ID="ID_1838782272" CREATED="1495318573728" MODIFIED="1495318624662" HGAP_QUANTITY="96.49999754130847 pt" VSHIFT_QUANTITY="-47.24999859184031 pt"/>
+</node>
+<node TEXT="Rolar p&#xe1;gina at&#xe9; &quot;Social Media Buttons&quot;" FOLDED="true" ID="ID_1778153482" CREATED="1495317478980" MODIFIED="1495318555478">
+<node TEXT="Abre Redes Sociais." ID="ID_1790775667" CREATED="1495318553625" MODIFIED="1495318602438" HGAP_QUANTITY="69.49999834597116 pt" VSHIFT_QUANTITY="44.999998658895535 pt"/>
+</node>
+<node TEXT="Bot&#xe3;o para ir no &quot;top&quot; da p&#xe1;gina" ID="ID_1459719085" CREATED="1495317737248" MODIFIED="1495317838142"/>
+</node>
+</node>
+</node>
+</map>
